0a83115c5f571825a2120417463245b5b3447060
Roman Schacherl <roman.schacherl@softaware.at>
2020-05-09 11:47:05 +0200

    Show sheet selection in editor

--- abc-music/out/extension.js
+++ abc-music/out/extension.js
@@ -6,8 +6,17 @@ function activate(context) {
     console.log('Congratulations, your extension "abc-music" is now active!');
     let disposable = vscode.commands.registerCommand('abc-music.showMusicsheet', () => {
         var _a, _b;
-        const panel = vscode.window.createWebviewPanel('musicSheet', 'Music Sheet', vscode.ViewColumn.Beside, {});
+        const panel = vscode.window.createWebviewPanel('musicSheet', 'Music Sheet', vscode.ViewColumn.Beside, {
+            enableScripts: true
+        });
         panel.webview.html = getWebviewContent((_b = (_a = vscode.window.activeTextEditor) === null || _a === void 0 ? void 0 : _a.document.getText()) !== null && _b !== void 0 ? _b : '');
+        panel.webview.onDidReceiveMessage(message => {
+            switch (message.command) {
+                case 'selection':
+                    jumpToPosition(message.start, message.stop);
+                    return;
+            }
+        }, undefined, context.subscriptions);
         // vscode.window.showOpenDialog
         vscode.workspace.onDidChangeTextDocument(eventArgs => {
             if (eventArgs.document.languageId == "abc") {
@@ -19,19 +28,22 @@ function activate(context) {
 }
 exports.activate = activate;
 function getWebviewContent(currentContent) {
-    var result = '';
+    var svgContent = '';
     let abcEngine;
     var user = {
         img_out: function (str) {
-            result += str;
+            svgContent += str;
         },
         anno_stop: function (type, start, stop, x, y, w, h, s) {
             if (["beam", "slur", "tuplet"].indexOf(type) >= 0) {
                 return;
             }
             //syms[start] = s		// music symbol
-            // create a rectangle
-            abcEngine.out_svg(`<rect class="abcr _${start}_" x="${x}" y="${y}" width="${w.toFixed(2)}" height="${abcEngine.sh(h).toFixed(2)}"/>\n`);
+            // create a rectangle as a clickable marker and add caret corresponding position as css class
+            abcEngine.out_svg(`<rect class="selMarker _${start}-${stop}_" x="`);
+            // out_sxsy translates x and y for correct positioning
+            abcEngine.out_sxsy(x, '" y="', y);
+            abcEngine.out_svg(`" width="${w.toFixed(2)}" height="${abcEngine.sh(h).toFixed(2)}"/>\n`);
         },
     };
     abcEngine = new abc.Abc(user);
@@ -44,14 +56,63 @@ function getWebviewContent(currentContent) {
 	  <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	  <title>ABC Music Sheet</title>
 	  <style type="text/css">
-	  .abcr {fill: #d00000; fill-opacity: 0; z-index: 15}
-	  </style>
+	  	.selMarker {fill: #d00000; fill-opacity: 0; z-index: 15}
+	  </style>	  
   </head>
   <body>
-	  ` + result + `
+	${svgContent}
+
+
+	<script type="text/javascript">
+		(function() {
+			const vscode = acquireVsCodeApi();
+					
+			document.addEventListener('click', function(e) {
+				e = e || window.event;
+				var target = e.target;   
+				
+				var element = event.target;
+				var cssClass = element.getAttribute('class');
+	
+				if (cssClass && cssClass.substr(0, 9) == "selMarker") {
+					var startStop = cssClass.slice(11, -1);
+					var start = Number(startStop.substr(0, startStop.indexOf('-')));
+					var stop = Number(startStop.substr(startStop.indexOf('-') + 1));
+					
+					vscode.postMessage({
+						command: 'selection',
+						start: start,
+						stop: stop
+					});
+				}
+			}, false);
+		}())
+
+	  </script>
   </body>
   </html>`;
 }
+function jumpToPosition(start, stop) {
+    var _a;
+    if (((_a = vscode.window.visibleTextEditors) === null || _a === void 0 ? void 0 : _a.length) != 1) {
+        return;
+    }
+    const editor = vscode.window.visibleTextEditors[0];
+    const currentContent = editor.document.getText();
+    const lines = currentContent === null || currentContent === void 0 ? void 0 : currentContent.split('\n');
+    let lineNumber = 1;
+    let currentCharacterCount = 0;
+    while ((currentCharacterCount + (lines[lineNumber - 1].length + 1)) < start) {
+        // add line length of current line number + add 1 (for \n)
+        currentCharacterCount += (lines[lineNumber - 1].length + 1);
+        lineNumber++;
+    }
+    let charactersBeforePosition = start - currentCharacterCount;
+    let selectionLength = stop - start;
+    editor.selection = new vscode.Selection(lineNumber - 1, charactersBeforePosition, lineNumber - 1, charactersBeforePosition + selectionLength);
+    vscode.window.activeTextEditor = editor;
+}
+exports.jumpToPosition = jumpToPosition;
 // this method is called when your extension is deactivated
 function deactivate() { }
 exports.deactivate = deactivate;
--- abc-music/src/extension.ts
+++ abc-music/src/extension.ts
@@ -11,12 +11,27 @@ export function activate(context: vscode.ExtensionContext) {
 			'musicSheet',
 			'Music Sheet',
 			vscode.ViewColumn.Beside,
-			{} 
+			{
+				enableScripts: true
+			}
 		);
 
 		panel.webview.html = getWebviewContent(vscode.window.activeTextEditor?.document.getText() ?? '');
 	
-		// vscode.window.showOpenDialog
+		panel.webview.onDidReceiveMessage(
+			message => {
+			  
+			  switch (message.command) {
+				case 'selection':
+				  jumpToPosition(message.start, message.stop);
+				  return;
+			  }
+			},
+			undefined,
+			context.subscriptions
+		);
+		
+		  // vscode.window.showOpenDialog
 
 		vscode.workspace.onDidChangeTextDocument(eventArgs => {
 			if (eventArgs.document.languageId == "abc") {
@@ -30,13 +45,13 @@ export function activate(context: vscode.ExtensionContext) {
 
 
 function getWebviewContent(currentContent: string) {
-	var result = '';
+	var svgContent = '';
 
 	let abcEngine: any;
 
 	var user = {
 		img_out: function (str: any) {
-			result += str;
+			svgContent += str;
 		},
 		anno_stop: function(type: string, start: number, stop: number, x: number, y: number, w: number, h: number, s: any) {
 			if (["beam", "slur", "tuplet"].indexOf(type) >= 0) {
@@ -45,8 +60,11 @@ function getWebviewContent(currentContent: string) {
 			
 			//syms[start] = s		// music symbol
 	
-			// create a rectangle
-			abcEngine.out_svg(`<rect class="abcr _${start}_" x="${x}" y="${y}" width="${w.toFixed(2)}" height="${abcEngine.sh(h).toFixed(2)}"/>\n`);
+			// create a rectangle as a clickable marker and add caret corresponding position as css class
+			abcEngine.out_svg(`<rect class="selMarker _${start}-${stop}_" x="`);
+			// out_sxsy translates x and y for correct positioning
+			abcEngine.out_sxsy(x, '" y="', y);
+			abcEngine.out_svg(`" width="${w.toFixed(2)}" height="${abcEngine.sh(h).toFixed(2)}"/>\n`);
 		},
 	};
 
@@ -62,13 +80,68 @@ function getWebviewContent(currentContent: string) {
 	  <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	  <title>ABC Music Sheet</title>
 	  <style type="text/css">
-	  .abcr {fill: #d00000; fill-opacity: 0; z-index: 15}
-	  </style>
+	  	.selMarker {fill: #d00000; fill-opacity: 0; z-index: 15}
+	  </style>	  
   </head>
   <body>
-	  ` + result + `
+	${svgContent}
+
+
+	<script type="text/javascript">
+		(function() {
+			const vscode = acquireVsCodeApi();
+					
+			document.addEventListener('click', function(e) {
+				e = e || window.event;
+				var target = e.target;   
+				
+				var element = event.target;
+				var cssClass = element.getAttribute('class');
+	
+				if (cssClass && cssClass.substr(0, 9) == "selMarker") {
+					var startStop = cssClass.slice(11, -1);
+					var start = Number(startStop.substr(0, startStop.indexOf('-')));
+					var stop = Number(startStop.substr(startStop.indexOf('-') + 1));
+					
+					vscode.postMessage({
+						command: 'selection',
+						start: start,
+						stop: stop
+					});
+				}
+			}, false);
+		}())
+
+	  </script>
   </body>
   </html>`;
+}
+
+export function jumpToPosition(start: number, stop: number) {
+	if (vscode.window.visibleTextEditors?.length != 1) {
+		return;
+	}
+
+	const editor = vscode.window.visibleTextEditors[0];
+
+	const currentContent:string = editor.document.getText();
+	const lines:string[] = currentContent?.split('\n');
+	let lineNumber: number = 1;
+	
+	let currentCharacterCount: number = 0;
+
+	while((currentCharacterCount + (lines[lineNumber - 1].length + 1)) < start) {
+		// add line length of current line number + add 1 (for \n)
+		currentCharacterCount += (lines[lineNumber - 1].length + 1);
+		lineNumber++;
+	}
+
+	let charactersBeforePosition = start-currentCharacterCount;
+
+	let selectionLength = stop-start;
+	editor.selection = new vscode.Selection(lineNumber - 1, charactersBeforePosition, lineNumber - 1, charactersBeforePosition+selectionLength);
+	
+	vscode.window.activeTextEditor = editor;
 }
 
 // this method is called when your extension is deactivated
